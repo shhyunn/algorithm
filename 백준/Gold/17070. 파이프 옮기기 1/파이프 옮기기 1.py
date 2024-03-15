8b752c5acffff626f533a14ab3bff5a21adf6e53
limsohyun <108120508+shhyunn@users.noreply.github.com>
2024-03-15 13:48:38 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -8,23 +8,19 @@ for _ in range(N):
     #1,1괴 1,2는 항상 빈칸 (파이프 현재 위치)
     #n,n으로 이동시키는 방법의 개수
 
-res = 0
-def recur(r,c,d):#r,c
-    global res
-    if r == N-1 and c == N-1:
-        res += 1
-        return
-    
-    if d != 2: #오른쪽
-        if c+1 < N and arr[r][c+1] == 0:
-            recur(r,c+1,0)
-    
-    if d != 0: #아래쪽
-        if r+1 < N and arr[r+1][c] == 0:
-            recur(r+1,c,2)
-    
-    if r+1 < N and c+1 < N and arr[r+1][c] == 0 and arr[r][c+1] == 0 and arr[r+1][c+1] == 0:
-        recur(r+1,c+1,1)
+dp = [[[0 for _ in range(3)] for _ in range(N)] for _ in range(N)]
+dp[0][1][0] = 1
+for i in range(N):
+    for j in range(N):
+        if i == 0 and j <= 1:
+            continue
+        if arr[i][j] == 0:
+            if j-1 >= 0:
+                dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][1]
 
-recur(0,1,0)
-print(res)
+            if i-1 >= 0 and j-1 >= 0 and arr[i-1][j] == 0 and arr[i][j-1] == 0:
+                dp[i][j][1] = dp[i-1][j-1][0] + dp[i-1][j-1][1] + dp[i-1][j-1][2]
+            if i-1 >= 0:
+                dp[i][j][2] = dp[i-1][j][1] + dp[i-1][j][2]
+
+print(sum(dp[N-1][N-1]))
