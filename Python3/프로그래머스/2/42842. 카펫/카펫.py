0dd5549b91a3e53483c5f358bba130dc2ecba66f
Lim Sohyun <108120508+shhyunn@users.noreply.github.com>
2024-11-18 01:43:28 +0900

    [level 2] Title: 카펫, Time: 0.14 ms, Memory: 10 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,12 @@
+def solution(brown, yellow):
+    answer = []
+    lst = []
+    # total = yellow
+    for i in range(1, yellow+1):
+        if yellow % i == 0:
+            j = yellow // i
+            
+            if i >= j and yellow + brown == (i+2) * (j+2):
+                return [i+2, j+2]
+        
+    # return answer
