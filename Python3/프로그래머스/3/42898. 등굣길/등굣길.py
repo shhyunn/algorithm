bdce41867821d7ec93ff6d5342ef7c7f88221d43
Lim Sohyun <108120508+shhyunn@users.noreply.github.com>
2024-11-19 01:05:34 +0900

    [level 3] Title: 등굣길, Time: 2.48 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/3/42898. 등굣길/등굣길.py
+++ Python3/프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,16 +1,22 @@
 def solution(m, n, puddles):
-    # Puddles를 집합으로 변환
-    puddle_set = {(a-1, b-1) for a, b in puddles}
+    answer = 0
+    dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
     
-    # 1차원 DP 배열 초기화
-    dp = [0] * m
-    dp[0] = 1  # 시작점
+    for puddle in puddles:
+        [a,b] = puddle #m, n
+        dp[b][a] = -1
     
-    for i in range(n):  # 행
-        for j in range(m):  # 열
-            if (j, i) in puddle_set:  # 물 웅덩이일 경우
-                dp[j] = 0
-            elif j > 0:  # 물 웅덩이가 아닌 경우
-                dp[j] += dp[j-1]
     
-    return dp[m-1] % 1000000007  # 최종 결과 반환
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if i == 1 and j == 1:
+                dp[1][1] = 1
+                continue
+            if dp[i][j] == -1:
+                dp[i][j] = 0
+                continue
+                
+            dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000007  # 위쪽에서 오는 경우의 수
+                
+    answer = dp[n][m]
+    return answer
