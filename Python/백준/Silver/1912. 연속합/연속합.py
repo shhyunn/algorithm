6272b6cc4348aaf7bfb6a5f99c465e908a81d1a6
Lim Sohyun <108120508+shhyunn@users.noreply.github.com>
2024-12-04 17:38:54 +0900

    [Silver II] Title: 연속합, Time: 88 ms, Memory: 38828 KB -BaekjoonHub

---
+++ Python/백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+arr = list(map(int, input().split(" ")))
+
+for i in range(1, N):
+    arr[i] = max(arr[i-1]+arr[i], arr[i])
+
+print(max(arr))
