9f5b73142e7aa50a91f6cf2f9eb47b7cafe64ab9
Lim Sohyun <108120508+shhyunn@users.noreply.github.com>
2025-02-18 02:04:35 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 45.34 ms, Memory: 37.7 MB -BaekjoonHub

--- Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,14 +1,41 @@
+from collections import defaultdict
 def solution(participant, completion):
-    
-    dict = {}
+    dic = defaultdict(int)
     temp = 0
-    for p in participant:
-        temp += hash(p)
-        dict[hash(p)] = p 
+    for s in participant:
+        temp += hash(s)
+        dic[hash(s)] = s
     
     for c in completion:
         temp -= hash(c)
-    res = dict[temp]
+    
+    res = dic[temp]
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#     dict = {}
+#     temp = 0
+#     for p in participant:
+#         temp += hash(p)
+#         dict[hash(p)] = p 
+    
+#     for c in completion:
+#         temp -= hash(c)
+#     res = dict[temp]
         
     
         
