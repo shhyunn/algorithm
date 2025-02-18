d83eabedb72de7679b8c852a0de07b7093abae9f
Lim Sohyun <108120508+shhyunn@users.noreply.github.com>
2025-02-19 01:18:51 +0900

    [level 2] Title: 프로세스, Time: 0.54 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/42587. 프로세스/프로세스.py
+++ Python3/프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,18 +1,58 @@
 def solution(priorities, location):
-    answer = [0] * len(priorities)
-    sequence = 1
-    queue = [i for i in range(len(priorities))]
+    process = [key for key in range(len(priorities))]
     
-    while queue:
-        result = 0
-        p = queue.pop(0)
-        for item in queue:
-            if priorities[item] > priorities[p]:
-                queue.append(p)
-                result = 1
-                break
-        if result == 0:
-            answer[p] = sequence
-            sequence += 1
+    locations = [0 for _ in range(len(priorities))]
+    
+    stack = []
+    sequence = 1
+    while process:
+        value = process.pop(0)
         
-    return answer[location]
+        skip = False
+        for other in process:
+            if priorities[value] < priorities[other]:
+                process.append(value)
+                skip = True
+                break
+        
+        if skip:
+            continue
+        
+        locations[value] = sequence
+        sequence += 1
+    
+    return locations[location]
+        
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#     answer = [0] * len(priorities)
+#     sequence = 1
+#     queue = [i for i in range(len(priorities))]
+    
+#     while queue:
+#         result = 0
+#         p = queue.pop(0)
+#         for item in queue:
+#             if priorities[item] > priorities[p]:
+#                 queue.append(p)
+#                 result = 1
+#                 break
+#         if result == 0:
+#             answer[p] = sequence
+#             sequence += 1
+        
+#     return answer[location]
