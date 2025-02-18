196e3ffe9a3e2cf5380dec671c99709c545a35a1
Lim Sohyun <108120508+shhyunn@users.noreply.github.com>
2025-02-19 01:37:07 +0900

    [level 2] Title: 주식가격, Time: 31.10 ms, Memory: 19.5 MB -BaekjoonHub

--- Python3/프로그래머스/2/42584. 주식가격/주식가격.py
+++ Python3/프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,23 +1,64 @@
 def solution(prices):
-    answer = [0] * len(prices)
+    #가격이 떨어지지 않은 기간은 몇초인가?
     stack = []
-    
-    for i,p in enumerate(prices):
+    answer = [0 for _ in range(len(prices))]
+    for i, p in enumerate(prices):
         if not stack:
-            stack.append((i,p))
+            stack.append(i)
             continue
         
-        if stack[-1][1] <= p:
-            stack.append((i,p))
-            continue
-        
-        while stack and stack[-1][1] > p:
-            ti, tp = stack.pop()
-            answer[ti] = i-ti
-        stack.append((i,p))
-    
+        if prices[stack[-1]] <= p:
+            stack.append(i)
+        else:
+            #마지막 스택 값보다 작을 경우
+            while stack:
+                if prices[stack[-1]] > p:
+                    l = stack.pop()
+                    answer[l] = i - l
+                else:
+                    break
+            stack.append(i)
     while stack:
-        ti, tp = stack.pop()
-        answer[ti] = len(prices) - ti - 1
-        
+        l = stack.pop()
+        answer[l] = len(prices) - l - 1
+    
     return answer
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#     answer = [0] * len(prices)
+#     stack = []
+    
+#     for i,p in enumerate(prices):
+#         if not stack:
+#             stack.append((i,p))
+#             continue
+        
+#         if stack[-1][1] <= p:
+#             stack.append((i,p))
+#             continue
+        
+#         while stack and stack[-1][1] > p:
+#             ti, tp = stack.pop()
+#             answer[ti] = i-ti
+#         stack.append((i,p))
+    
+#     while stack:
+#         ti, tp = stack.pop()
+#         answer[ti] = len(prices) - ti - 1
+        
+#     return answer
