2d7fae2cd9b7f268be40e076bed3ec5c1638a9c5
Lim Sohyun <108120508+shhyunn@users.noreply.github.com>
2025-02-19 23:10:59 +0900

    [level 2] Title: 더 맵게, Time: 1682.71 ms, Memory: 51.9 MB -BaekjoonHub

--- Python3/프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ Python3/프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,22 +1,51 @@
 import heapq
 
 def solution(scoville, K):
-    answer = 0  
+    #모든 음식의 스코빌지수를 K이상으로 만들기 위해 섞어야 하는 최소 횟수?
+    stack = []
+    res = 0
     heapq.heapify(scoville)
     
     while scoville:
         if scoville[0] >= K:
             break
-        
+
         if len(scoville) < 2:
-            answer = -1
-            break
-        
-        a = heapq.heappop(scoville) #가장 안매운 음식
-        b = heapq.heappop(scoville) #두번째로 안매운 음식
-        
-        c = a + b*2
+            return -1
+        a = heapq.heappop(scoville)
+        b = heapq.heappop(scoville)
+        c = a + b * 2
         heapq.heappush(scoville, c)
-        answer += 1
+        res += 1
+    
+    return res
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#     answer = 0  
+#     heapq.heapify(scoville)
+    
+#     while scoville:
+#         if scoville[0] >= K:
+#             break
+        
+#         if len(scoville) < 2:
+#             answer = -1
+#             break
+        
+#         a = heapq.heappop(scoville) #가장 안매운 음식
+#         b = heapq.heappop(scoville) #두번째로 안매운 음식
+        
+#         c = a + b*2
+#         heapq.heappush(scoville, c)
+#         answer += 1
             
-    return answer
+#     return answer
