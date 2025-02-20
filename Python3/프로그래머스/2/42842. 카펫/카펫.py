58d0c7438104c1f81afcce0b822f731bcfb3dcc6
Lim Sohyun <108120508+shhyunn@users.noreply.github.com>
2025-02-21 00:58:35 +0900

    [level 2] Title: 카펫, Time: 0.12 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/42842. 카펫/카펫.py
+++ Python3/프로그래머스/2/42842. 카펫/카펫.py
@@ -1,6 +1,31 @@
 def solution(brown, yellow):
+    #카펫의 노란색과 갈색으로 색칠된 격자의 개수는 기억했으나, 전체 카펫의 크기는 기억하지 못함
+    #카펫의 가로, 세로 크기를 순서대로 배열에 담기
+    
     for i in range(1, yellow+1):
-        if yellow % i == 0:
-            j = yellow // i
-            if i >= j and yellow + brown == (i+2) * (j+2):
-                return [i+2, j+2]
+        if yellow % i != 0:
+            continue
+            
+        j = yellow // i
+        if (i+2) * (j+2) - i*j == brown:
+            return [j+2, i+2]
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # for i in range(1, yellow+1):
+    #     if yellow % i == 0:
+    #         j = yellow // i
+    #         if i >= j and yellow + brown == (i+2) * (j+2):
+    #             return [i+2, j+2]
