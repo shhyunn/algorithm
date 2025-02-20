6738c45f827b483f641fbed0ae69ef73b493c463
Lim Sohyun <108120508+shhyunn@users.noreply.github.com>
2025-02-20 23:28:18 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/1/42748. K번째수/K번째수.py
+++ Python3/프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,10 +1,33 @@
 def solution(array, commands):
+    #i번째 숫자부터 j번째 숫자까지 자르고 정렬 -> K번째 있는 수
+    #i,j,k
     answer = []
-    
-    for command in commands:
-        [i,j,k] = command
-        temp_array = array[i-1:j]
-        temp_array.sort()
-        answer.append(temp_array[k-1])
+    for i,j,k in commands:
+        temp = array[i-1:j]
+        temp.sort()
+        answer.append(temp[k-1])
     
     return answer
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#     answer = []
+    
+#     for command in commands:
+#         [i,j,k] = command
+#         temp_array = array[i-1:j]
+#         temp_array.sort()
+#         answer.append(temp_array[k-1])
+    
+#     return answer
