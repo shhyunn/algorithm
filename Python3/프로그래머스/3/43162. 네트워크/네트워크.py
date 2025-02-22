1808d68f08f50b2db6698fecd813900787e9d009
Lim Sohyun <108120508+shhyunn@users.noreply.github.com>
2025-02-22 20:59:17 +0900

    [level 3] Title: 네트워크, Time: 0.49 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/3/43162. 네트워크/네트워크.py
+++ Python3/프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,19 +1,49 @@
 from collections import deque
+
 def solution(n, computers):
-    answer = 0
+    #네트워크의 개수
     visited = [False] * n
+    res = 0
     
-    for i in range(n):
-        if visited[i]: #이미 방문했으면 패스
+    for k in range(n):
+        if visited[k]:
             continue
-        visited[i] = True
-        answer += 1
-        stack = deque([i])
+        visited[k] = True
+        stack = deque([k])
+        res += 1
         while stack:
-            s = stack.popleft()
-            for j in range(n):
-                if s != j and computers[s][j] == 1 and not visited[j]:
-                    visited[j] = True
-                    stack.append(j)
+            curr = stack.popleft() 
+            for i,v in enumerate(computers[curr]):
+                if not visited[i] and v == 1:
+                    visited[i] = True
+                    stack.append(i)
+    return res
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#     answer = 0
+#     visited = [False] * n
+    
+#     for i in range(n):
+#         if visited[i]: #이미 방문했으면 패스
+#             continue
+#         visited[i] = True
+#         answer += 1
+#         stack = deque([i])
+#         while stack:
+#             s = stack.popleft()
+#             for j in range(n):
+#                 if s != j and computers[s][j] == 1 and not visited[j]:
+#                     visited[j] = True
+#                     stack.append(j)
                        
-    return answer
+#     return answer
