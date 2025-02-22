b96600c8b89b536bdc8a114214ec93bb6310ba34
Lim Sohyun <108120508+shhyunn@users.noreply.github.com>
2025-02-22 18:27:39 +0900

    [level 3] Title: 정수 삼각형, Time: 55.22 ms, Memory: 20.2 MB -BaekjoonHub

--- Python3/프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ Python3/프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,19 +1,54 @@
 def solution(triangle):
-    #대각선으로 한칸 오른쪽 / 왼쪽으로 한칸 오른쪽
-    answer = 0
-    h = len(triangle)
-    dp = [[0 for _ in range(h)] for _ in range(h)]
-    dp[0][0] = triangle[0][0]
+    #경로에서 거쳐간 숫자의 합이 가장 큰 경우
+    l = len(triangle[-1])
+    dp = [[0 for _ in range(l)] for _ in range(l)]
     
-    for i in range(1, h):
+    for i in range(l):
         for j in range(i+1):
-            if j < 1:
-                dp[i][j] = dp[i-1][j] + triangle[i][j]
-            elif j == i:
-                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
-            else:
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j-1]) + triangle[i][j]
-                
-    answer = max(dp[h-1])
+            if i == 0:
+                dp[i][j] = triangle[i][j]
+                continue
             
-    return answer
+            if j == 0:
+                dp[i][j] = dp[i-1][j] + triangle[i][j]
+                continue
+            
+            if j == i:
+                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+                continue
+            
+            dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
+            
+    return max(dp[l-1])
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#     #대각선으로 한칸 오른쪽 / 왼쪽으로 한칸 오른쪽
+#     answer = 0
+#     h = len(triangle)
+#     dp = [[0 for _ in range(h)] for _ in range(h)]
+#     dp[0][0] = triangle[0][0]
+    
+#     for i in range(1, h):
+#         for j in range(i+1):
+#             if j < 1:
+#                 dp[i][j] = dp[i-1][j] + triangle[i][j]
+#             elif j == i:
+#                 dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+#             else:
+#                 dp[i][j] = max(dp[i-1][j], dp[i-1][j-1]) + triangle[i][j]
+                
+#     answer = max(dp[h-1])
+            
+#     return answer
