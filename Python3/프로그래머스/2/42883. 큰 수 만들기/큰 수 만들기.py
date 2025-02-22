53fadb22762c972671ff55f179258785c58996cf
Lim Sohyun <108120508+shhyunn@users.noreply.github.com>
2025-02-22 15:48:34 +0900

    [level 2] Title: 큰 수 만들기, Time: 107.76 ms, Memory: 13 MB -BaekjoonHub

--- Python3/프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ Python3/프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,18 +1,58 @@
 def solution(number, k):
+    #어떤 숫자에서 k개의 수를 제거했을 때 얻을 수 있는 가장 큰 숫자
+    #뒷 수가 현재 수보다 크다면 제거하는게 맞음 이걸 반복적으로...
+    
+    curr = k
     stack = []
-    for n in number:
+    for i, number in enumerate(number):
         if not stack:
-            stack.append(n)
+            stack.append(number)
             continue
-        while stack and n > stack[-1] and k > 0:
+        
+        while stack and stack[-1] < number and curr > 0:
             stack.pop()
-            k -= 1
-        
-        stack.append(n)
-        
-    while k > 0:
-        stack.pop()
-        k -= 1
+            curr -= 1
             
-    answer = ''.join(stack)
-    return answer
+        else:
+            stack.append(number)
+    
+    while curr > 0:
+        stack.pop()
+        curr -= 1
+    
+    return "".join(stack)
+            
+            
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#     stack = []
+#     for n in number:
+#         if not stack:
+#             stack.append(n)
+#             continue
+#         while stack and n > stack[-1] and k > 0:
+#             stack.pop()
+#             k -= 1
+        
+#         stack.append(n)
+        
+#     while k > 0:
+#         stack.pop()
+#         k -= 1
+            
+#     answer = ''.join(stack)
+#     return answer
