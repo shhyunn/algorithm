206dc89297cff58d3a586f7b25d649ffab1ef050
Lim Sohyun <108120508+shhyunn@users.noreply.github.com>
2025-03-19 17:06:28 +0900

    [level 2] Title: 양궁대회, Time: 540.48 ms, Memory: 9.17 MB -BaekjoonHub

--- Python3/프로그래머스/2/92342. 양궁대회/양궁대회.py
+++ Python3/프로그래머스/2/92342. 양궁대회/양궁대회.py
@@ -5,28 +5,28 @@ def get_cur_point(apeach, lion):
     lion_point = 0
     
     for i in range(11):
-        if apeach[i] == 0 and lion[i] == 0:
+        if apeach[i] == 0 and lion[i] == 0: #0이면 아무도 점수를 못얻음
             continue
-        if apeach[i] >= lion[i]:
-            apeach_point += 10 - i
+        if apeach[i] >= lion[i]: #어피치가 더 많이 얻는다면
+            apeach_point += 10 - i #어피치 포인트 더하기
         else:
-            lion_point += 10 - i
+            lion_point += 10 - i #라이언 포인트 더하기
             
-    return lion_point - apeach_point        
+    return lion_point - apeach_point #그 둘의 차        
 
 
 def solution(n, info):
     best_point = 0
     best_result = [0] * 11
     
-    for comb in combinations_with_replacement(range(11), n):
+    for comb in combinations_with_replacement(range(11), n): #0~10까지 중에 n발을 어떻게 쏘았는가?
         cur_result = [0] * 11
         
-        for num in comb:
+        for num in comb: #각 발에 따라 현재 결과에 +1
             cur_result[num] += 1
             
-        cur_point = get_cur_point(info, cur_result) 
-        if (cur_point > best_point) or (cur_point == best_point and cur_result[::-1] > best_result[::-1]):
+        cur_point = get_cur_point(info, cur_result) #어피치 발사 결과, cur_result는 라이언 발사 결과
+        if (cur_point > best_point) or (cur_point == best_point and cur_result[::-1] > best_result[::-1]): #현재 best_point보다 차가 크거나 혹은 현재 결과가 더 낮은 숫자를 많이 맞췃을 경우
             best_point = cur_point
             best_result = cur_result[:] # deepcopy
         
