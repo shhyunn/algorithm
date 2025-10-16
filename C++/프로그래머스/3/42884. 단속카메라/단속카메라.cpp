ece6210a671492c559d4d077d11516f834c3e4ab
Lim Sohyun <108120508+shhyunn@users.noreply.github.com>
2025-10-16 22:31:49 +0900

    [level 3] Title: 단속카메라, Time: 0.59 ms, Memory: 3.95 MB -BaekjoonHub

--- C++/프로그래머스/3/42884. 단속카메라/단속카메라.cpp
+++ C++/프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -1,34 +1,29 @@
-#include <string>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 //모든 차량이 한 번의 단속용 카메라를 만나도록 하려면 최소 몇 대의 카메라가 필요한가??
 //나간 지점 기준으로 정렬 + 처음 나간 지점에 카메라 설치
 
-bool sorting(vector<int>& a, vector<int>& b) {
+bool compare(vector<int>& a, vector<int>& b) {
     return a[1] < b[1];
 }
 
 int solution(vector<vector<int>> routes) {
-    int answer = 0;
-    sort(routes.begin(), routes.end(), sorting);
-    int cnt = 0;
-    int curr = -1;
+    sort(routes.begin(), routes.end(), compare);
     
+    int curr_end = -30001;
+    int answer = 0;
     for (auto& r : routes) {
-        if (curr == -1) {
-            cnt++;
-            curr = r[1];
+        int start = r[0];
+        int end = r[1];
+        
+        if (curr_end < start) {
+            answer++;
+            curr_end = end;
             continue;
         }
         
-        if (r[0] <= curr) continue;
-        else {
-            cnt++;
-            curr = r[1];
-            continue;
-        }
+        
     }
-    return cnt;
+    return answer;
 }
