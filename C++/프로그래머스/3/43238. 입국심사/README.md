84f622114b0db180b555b8aec6b9164964ba4d3b
Lim Sohyun <108120508+shhyunn@users.noreply.github.com>
2025-10-18 01:28:22 +0900

    [level 3] Title: 입국심사, Time: 44.29 ms, Memory: 6.66 MB -BaekjoonHub

--- C++/프로그래머스/3/43238. 입국심사/README.md
+++ C++/프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=cpp) 
 
 ### 성능 요약
 
-메모리: 6.8 MB, 시간: 45.84 ms
+메모리: 6.66 MB, 시간: 44.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 25일 19:02:22
+2025년 10월 18일 01:28:20
 
 ### 문제 설명
 
--- C++/프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ C++/프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -1,32 +1,26 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <climits>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 long long solution(int n, vector<int> times) {
+    long long start = 1;
+    long long end = (long long)*max_element(times.begin(), times.end()) * n;
     
-    long long left = 1;
-    long long right = *max_element(times.begin(), times.end()) * (long)n;
-    long long answer = right;
-    long long tmp;
-    
-    while (left <= right) {
-        long long mid = left + (right - left) / 2;
-        tmp = 0;
-        for (auto& t : times) {
-            tmp += (mid / t);
+    long long mid;
+    long long answer = LLONG_MAX;
+    while (start <= end) {
+        mid = (start + end) / 2;
+        long long cnt = 0;
+        for (int& t : times) {
+            cnt += (mid / t);
         }
         
-        if (n <= tmp) {
+        if (cnt >= n) {
+            end = mid - 1;
             answer = min(answer, mid);
-            right = mid - 1;
         } else {
-            left = mid + 1;
+            start = mid + 1;
         }
     }
-    
     return answer;
 }
